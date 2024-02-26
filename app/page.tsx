0ca77f91917ab2cf278e7b86670e448f6ecad88e
k-shindo <k-shindo@media-plex.net>
2024-02-26 22:34:21 +0900

    ✨ auth pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button size='lg'>Click me</Button>
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboad</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
